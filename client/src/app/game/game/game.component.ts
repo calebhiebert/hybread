d01eb24a87c291ddb8a6e274208be3d681e5bf32
Caleb Hiebert <calebkhiebert@gmail.com>
2018-11-02 14:59:49 -0500

    create phaser game

--- client/src/app/game/game/game.component.ts
+++ client/src/app/game/game/game.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { PreloadScene } from 'src/game/Preload.scene';
+import * as Phaser from 'phaser';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements AfterViewInit {
+  private ASPECT = 16.0 / 9.0;
 
-  constructor() { }
+  @ViewChild('gamecontainer')
+  private gameContainer: HTMLElement;
 
-  ngOnInit() {
+  @ViewChild('gamecanvas')
+  private gameCanvas: ElementRef<HTMLCanvasElement>;
+
+  private game: Phaser.Game;
+  private resizeTimer: any;
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    const config: GameConfig = {
+      width: window.innerWidth,
+      height: window.innerWidth / this.ASPECT,
+      // parent: this.gameContainer,
+      canvas: this.gameCanvas.nativeElement,
+      type: Phaser.AUTO,
+      scene: PreloadScene,
+    };
+
+    this.game = new Phaser.Game(config);
+    console.log(this.game);
+
+    window.addEventListener('resize', () => {
+      this.resizeEventHandler();
+    });
+
+    this.resizeGameWindow();
   }
 
+  private resizeEventHandler() {
+    if (this.resizeTimer !== undefined) {
+      return;
+    }
+
+    this.resizeTimer = setTimeout(() => {
+      this.resizeTimer = undefined;
+      this.resizeGameWindow();
+    }, 10);
+  }
+
+  private resizeGameWindow() {
+    if (!this.game) {
+      console.warn('Game not present');
+      return;
+    }
+
+    let width = window.innerWidth;
+    let height = width / this.ASPECT;
+
+    if (height > window.innerHeight) {
+      height = window.innerHeight;
+      width = height * this.ASPECT;
+    }
+
+    this.game.resize(width, height);
+  }
 }
---
+++ client/src/game/Preload.scene.ts
@@ -0,0 +1,13 @@
+import * as Phaser from 'phaser';
+
+export class PreloadScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'preload',
+    });
+  }
+
+  preload(): void {}
+
+  create(): void {}
+}
