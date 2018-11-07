8ad5789cb8ae8010e78e2ec330739c167f90d5b9
Caleb Hiebert <calebkhiebert@gmail.com>
2018-11-07 11:08:49 -0600

    add some store css

--- client/src/app/game/store/store.component.ts
+++ client/src/app/game/store/store.component.ts
@@ -4,7 +4,7 @@ import { HybreadAPI, IITem } from 'src/api';
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.css'],
+  styleUrls: ['./store.component.scss'],
 })
 export class StoreComponent implements OnInit {
   // The list of purchasable items
