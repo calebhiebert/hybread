bfc02cfe73ada32159dc7956b137f46588dfd000
David Budnick <david@budnick.ca>
2018-11-06 21:16:07 -0600

    Milk Added

---
+++ server/baker/milk.go
@@ -0,0 +1,54 @@
+/*
+	milk.go
+
+	This file conatins the implimentations for the milk items
+
+	Available Flour Types:
+	- Cow's Milk
+	- Goat's Milk
+	- Basically Cheese
+	- Demon's Milk
+	- Milk of Magnesia
+	- Dragon's Milk
+*/
+
+package baker
+
+type Milk struct {
+	MilkType string
+	Amount   int
+}
+
+func (w Milk) mix(second int, bread *Bread) *Bread {
+	return bread
+}
+
+func (w Milk) rise(minute int, bread *Bread) *Bread {
+	return bread
+}
+
+func (w Milk) knead(second int, bread *Bread) *Bread {
+	return bread
+}
+
+func (w Milk) pan(bread *Bread) *Bread {
+	return bread
+}
+
+func (w Milk) bake(minute int, bread *Bread) *Bread {
+	switch w.FlourType {
+	case "Cow's Milk":
+
+	case "Goat's Milk":
+
+	case "Basically Cheese":
+
+	case "Demon's Milk":
+
+	case "Milk of Magnesia":
+
+	case "Dragon's Milk":
+
+	}
+	return bread
+}
